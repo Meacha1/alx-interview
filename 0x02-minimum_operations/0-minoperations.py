d994e2ed747ffabdfecba5ee67a86534074c88b3
Meacha1 <meachattd@gmail.com>
2023-07-13 19:49:18 +0300

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Calculates minimum number of copy/paste operations 
-to get n H characters"""
+"""Calculates minimum number of copy/paste operations
+to get n H characters
+"""
 
 import math
 
 
 def minOperations(n):
     """Computes minimum copy/paste operations for n chars
-    
+
     Args:
         n (int): Number of H characters to create
-        
+
     Returns:
         int: Minimum number of copy/paste operations
     """
-    
+
     if n <= 1:
         return n
-    
+
     for i in range(int(math.sqrt(n)) + 1, 0, -1):
         if n % i == 0:
             return i + minOperations(int(n / i))
