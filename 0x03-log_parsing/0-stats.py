e115d3f1c79cfef45e862ed762e710b20f4575ba
Meacha1 <meachattd@gmail.com>
2023-07-18 11:34:08 -0700

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,50 +2,43 @@
 """
 Script that reads stdin line by line and computes metrics.
 """
+
 import sys
 
+metrics = {
+    'total_size': 0,
+    'status_codes': {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    },
+    'line_count': 0
+}
 
-def print_stats(file_size, status_counts):
-    """
-    Prints the file size and status code counts.
-    """
-    print("File size: {}".format(file_size))
-    for status_code, count in sorted(status_counts.items()):
-        print("{}: {}".format(status_code, count))
+try:
+    for line in sys.stdin:
+        parts = line.strip().split(" ")
+        if len(parts) == 9:
+            ip, _, _, status, size = parts[0], parts[8], parts[7], int(parts[6]), int(parts[8])
+            if status in metrics['status_codes']:
+                metrics['total_size'] += size
+                metrics['status_codes'][status] += 1
+            metrics['line_count'] += 1
+        if metrics['line_count'] % 10 == 0:
+            print("File size: {}".format(metrics['total_size']))
+            for code, count in sorted(metrics['status_codes'].items()):
+                if count > 0:
+                    print("{}: {}".format(code, count))
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {}".format(metrics['total_size']))
+    for code, count in sorted(metrics['status_codes'].items()):
+        if count > 0:
+            print("{}: {}".format(code, count))
 
-
-def parse_line(line):
-    """
-    Parses a line and extracts the file size and status code.
-    Returns a tuple of (file_size, status_code).
-    """
-    parts = line.split(" ")
-    if len(parts) >= 7:
-        file_size = int(parts[-1])
-        status_code = int(parts[-2])
-        return file_size, status_code
-    return None, None
-
-
-def compute_metrics():
-    """
-    Computes the metrics from stdin.
-    """
-    total_file_size = 0
-    status_counts = {}
-
-    try:
-        line_count = 0
-        for line in sys.stdin:
-            file_size, status_code = parse_line(line.strip())
-            if file_size is not None and status_code is not None:
-                total_file_size += file_size
-                status_counts[status_code] = status_counts.get(status_code, 0) + 1
-
-            line_count += 1
-            if line_count % 10 == 0:
-                print_stats(total_file_size, status_counts)
-
-    except KeyboardInterrupt:
-        print_stats(total_file_size, status_counts)
-        raise
