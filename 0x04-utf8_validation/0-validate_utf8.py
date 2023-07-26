1ab622fbe4eb73dfc9c818bf3bf32dd89675a72f
Meacha1 <meachattd@gmail.com>
2023-07-26 22:30:58 +0300

    test4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,20 @@
 
 def validUTF8(data):
     '''Method to check if a given data set represents a valid UTF-8 encoding'''
-    if not data:
-        return False
-    count = 0
-    for byte in data:
-        if count == 0:
-            if byte >> 5 == 0b110:
-                count = 1
-            elif byte >> 4 == 0b1110:
-                count = 2
-            elif byte >> 3 == 0b11110:
-                count = 3
-            elif byte >> 7 == 1:
+    num_bytes = 0
+    for num in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while mask & num:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if num >> 6 != 2:
                 return False
-            count -= 1
-    return count == 0
+        num_bytes -= 1
+    return num_bytes == 0
+
