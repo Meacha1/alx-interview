81179fa3040f3c9b75833678d44d979055a40a69
Meacha1 <meachattd@gmail.com>
2023-08-03 16:57:09 +0300

    test

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -32,4 +32,21 @@ def solve_nqueens(n):
         backtrack(row+1)
 
   backtrack(0)
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
   
+  n = 0
+  try:
+    n = int(sys.argv[1])  
+  except:
+    print("N must be a number")
+    sys.exit(1)  
+  
+  if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+  solve_nqueens(n)
