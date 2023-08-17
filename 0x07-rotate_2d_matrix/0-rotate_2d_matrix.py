94048be84acfaa1f76d1b6b41366e8e347a2b5e5
Meacha1 <meachattd@gmail.com>
2023-08-17 22:56:13 +0300

    2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -21,11 +21,3 @@ def rotate_2d_matrix(matrix):
             matrix[last - offset][first] = matrix[last][last - offset]
             matrix[last][last - offset] = matrix[i][last]
             matrix[i][last] = top
-
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    print(matrix)
