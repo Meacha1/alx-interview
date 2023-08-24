f6448914910e736f65a9c594c96ce3036dd40c7f
Meacha1 <meachattd@gmail.com>
2023-08-24 20:56:19 +0300

    final

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Given a pile of coins of different values, determine the fewest 
+Given a pile of coins of different values, determine the fewest
 number of coins needed to meet a given amount total
 '''
 
