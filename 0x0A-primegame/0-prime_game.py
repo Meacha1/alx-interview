9800d35d3327f97e4af332ab241031ac49061a66
Meacha1 <meachattd@gmail.com>
2023-09-07 21:11:45 +0300

    prime

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def isWinner(x, nums):
+    if not nums or x < 1:
+        return None
+
+    wins = {"Maria": 0, "Ben": 0}
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            wins["Ben"] += 1
+        else:
+            wins["Maria"] += 1
+
+    if wins["Maria"] > wins["Ben"]:
+        return "Maria"
+    elif wins["Ben"] > wins["Maria"]:
+        return "Ben"
+    else:
+        return None
